595b95fcf3ec647c6bebb8510be26a67ead4538b
Julian DeGroot-Lutzner <jdegroot@students.pitzer.edu>
2020-11-30 14:50:10 -0500

    add basic search functionality - user can enter date, from, and to and receive flights - found trivial bug in seeds file but not fixed - practiced putting logic in Model file - struggled with understanding Ajax form submission

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -10,4 +10,33 @@ class Flight < ApplicationRecord
     errors.add(:from, 'must be different than \'to\'')
     errors.add(:to, 'must be different than \'from\'')
   end
+
+  # Notes:
+  # 1) I find the hash so much more readable than Strings with '?'
+  # 2) returning Flight.none avoids later checks if @flights exists
+  # 3) could use #readonly bc. i'm not updating model
+  def self.search(params)
+    return Flight.none unless params[:search]
+
+    Flight.where(from_id: params[:flight][:from_id],
+                 to_id: params[:flight][:to_id],
+                 date: build_date(params).beginning_of_day..build_date(params).end_of_day)
+          .order(:date)
+  end
+
+  def readable_time
+    date.strftime('%I:%M %p')
+  end
+
+  def readable_date
+    date.strftime('%A, %B %e, %Y')
+  end
+
+  private 
+
+
+  def self.build_date(params)
+    Date.new(params[:date][:year].to_i, params[:date][:month].to_i, params[:date][:day].to_i)
+  end
+
 end
