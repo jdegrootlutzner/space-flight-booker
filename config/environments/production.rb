24f5c4a62a5609cde76c5f08088accb6f8268bc4
Julian DeGroot-Lutzner <jdegroot@students.pitzer.edu>
2020-12-15 10:15:44 -0500

    set default url options in development

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -104,4 +104,6 @@ Rails.application.configure do
      :authentication => :plain,
      :enable_starttls_auto => true
    }
+   config.action_mailer.default_url_options = { host: 'lit-citadel-48167.herokuapp.com'}
+
 end
